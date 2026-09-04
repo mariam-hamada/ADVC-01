07fa8230e35b66fba8dcdc1b032ac60ae0e6c692
mariam-hamada <168047165+mariam-hamada@users.noreply.github.com>
2026-09-04 03:01:28 +0300

    Question1

--- Assignment10/Program.cs
+++ Assignment10/Program.cs
@@ -2,9 +2,14 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+
+        #region Question 1
+
+        //Q1: What is a generic class? Why use generics?
+        //A generic class is a class where the data type is not specified when the class is created. Instead, the type is specified when the class is used.
+        //We use generics to write one class or method that can work with different data types instead of writing separate code for each type.
+
+        #endregion
+
     }
 }
