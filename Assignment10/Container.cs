beeb9832f2d4885ae0b2398a724d669b9bcbae9c
mariam-hamada <168047165+mariam-hamada@users.noreply.github.com>
2026-09-04 03:15:58 +0300

    Question 2

---
+++ Assignment10/Container.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Assignment10
+//{
+//    class Container<T>
+//    {
+//        private List<T> items = new List<T>();
+
+//        public void Add(T item)
+//        {
+//            items.Add(item);
+//        }
+
+//        public T Get(int index)
+//        {
+//            if (index >= 0 && index < items.Count)
+//            {
+//                return items[index];
+//            }
+//            return default;
+//        }
+//    }
+//}
+
--- Assignment10/Program.cs
+++ Assignment10/Program.cs
@@ -2,14 +2,32 @@
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
 
-        #region Question 1
+            #region Question 1
 
-        //Q1: What is a generic class? Why use generics?
-        //A generic class is a class where the data type is not specified when the class is created. Instead, the type is specified when the class is used.
-        //We use generics to write one class or method that can work with different data types instead of writing separate code for each type.
+            //Q1: What is a generic class? Why use generics?
+            //A generic class is a class where the data type is not specified when the class is created. Instead, the type is specified when the class is used.
+            //We use generics to write one class or method that can work with different data types instead of writing separate code for each type.
 
-        #endregion
+            #endregion
 
+            #region Question 2
+
+            //Q2: Write a generic class Container<T> with Add and Get methods.
+            //Container<int> numbers = new Container<int>();
+            //numbers.Add(10);
+            //numbers.Add(20);
+            //numbers.Add(30);
+            //Console.WriteLine(numbers.Get(0));
+            //Console.WriteLine(numbers.Get(1));
+            //Console.WriteLine(numbers.Get(2));
+            //Console.WriteLine(numbers.Get(3));
+
+
+            #endregion
+
+        }
     }
 }
